53c08a9828cf8f02119ff8e20829cbd2cf9d15b4
Dzmitry Sashcheka <sheensheella@gmail.com>
2024-09-25 20:06:27 +0300

    Deep copy + new property

---
+++ lesson-02/07.js
@@ -0,0 +1,17 @@
+let passportMarried = {
+  name: 'Petr',
+  surname: 'Petrov',
+  address: {
+    country: 'USA',
+    city: 'Bobryisk',
+  },
+}
+
+let copyPassportMarried = {
+  ...passportMarried,
+  married: true,
+  address: { ...passportMarried.address },
+}
+
+console.log(passportMarried)
+console.log(copyPassportMarried)
